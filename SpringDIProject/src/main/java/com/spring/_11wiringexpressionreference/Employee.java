300b36a8b343b9d24d5ac1603bf49b7b391585c4
Taher <Taher@192.168.1.7>
2019-01-01 20:19:39 +0530

    Spring

--- SpringDIProject/src/main/java/com/spring/_11wiringexpressionreference/Employee.java
+++ SpringDIProject/src/main/java/com/spring/_11wiringexpressionreference/Employee.java
@@ -5,6 +5,7 @@ public class Employee {
 	private String name;
 	private Address address;
 	private String nameRefProperties;
+	private String cityRefMethod;
 
 	public void setName(String name) {
 		this.name = name;
@@ -18,9 +19,13 @@ public class Employee {
 		this.nameRefProperties = nameRefProperties;
 	}
 
+	public void setCityRefMethod(String cityRefMethod) {
+		this.cityRefMethod = cityRefMethod;
+	}
+
 	public void employeeAddress() {
 		System.out.println("Employee name is " + name + " and address is "
-				+ address.empAddress() + " " + nameRefProperties);
+				+ address.empAddress() + " " + nameRefProperties + " " +cityRefMethod);
 	}
 
 }
